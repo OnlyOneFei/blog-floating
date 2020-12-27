0bf2cde179e252fc32aa7bc11165328727c462c6
FeiYu <844797191@qq.com>
2020-12-27 22:01:58 +0800

    初始化项目

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <!--template 下只能一个节点-->
+    <div>
+        login
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
